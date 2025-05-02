92e6c816b87daf48ecf1b0012749d9a63db92e8f
lanouar <anouar.khaled2002@gmail.com>
2025-05-02 23:54:40 +0200

    change the  LSTM model for activity recognition

--- Model _using_mediapipe/main.py
+++ Model _using_mediapipe/main.py
@@ -1,7 +1,39 @@
-from model.train_model import train
-from model.predict import predict
-import os
-path ="testdata/test6.mp4"
-train()
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import json
 
 
+df = pd.read_csv('Model _using_mediapipe/keypoints6 (1).csv')
+
+
+# Suppression des lignes où la personne est debout : angle trop ouvert
+#df = df[~((df['label'] == 'barbell biceps curl') & (df['LEFT_SHOULDER_LEFT_ELBOW_LEFT_WRIST_angle'] > 175))]
+#df=df[df['label']!="hammer curl"]
+# Sauvegarde du dataset nettoyé
+data=df[['label','LEFT_SHOULDER_LEFT_ELBOW_LEFT_WRIST_angle',
+       'RIGHT_SHOULDER_RIGHT_ELBOW_RIGHT_WRIST_angle',
+       'LEFT_HIP_LEFT_KNEE_LEFT_ANKLE_angle',
+       'RIGHT_HIP_RIGHT_KNEE_RIGHT_ANKLE_angle']]
+def normalize_angles(df):
+    """
+    Normalise les angles articulaires entre -1 et 1 en conservant la relation cyclique des angles
+    """
+    # Création d'une copie pour ne pas modifier l'original
+    normalized_df = df.copy()
+
+    # Normalisation sinus/cosinus pour préserver la circularité des angles
+    for col in ['LEFT_SHOULDER_LEFT_ELBOW_LEFT_WRIST_angle',
+       'RIGHT_SHOULDER_RIGHT_ELBOW_RIGHT_WRIST_angle',
+       'LEFT_HIP_LEFT_KNEE_LEFT_ANKLE_angle',
+       'RIGHT_HIP_RIGHT_KNEE_RIGHT_ANKLE_angle']:
+        # Conversion en radians
+        # Normalisation circulaire
+        normalized_df[col]= df[col]/180
+
+        
+
+    return normalized_df
+df=normalize_angles(data)
+df.to_csv('Model _using_mediapipe/keypoints6 (1).csv', index=False)
